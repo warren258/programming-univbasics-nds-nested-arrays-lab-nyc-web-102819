6784f6939106f6f6b7bace94693637144af60659
Warren McDonald <warrenjmac@mail.com>
2019-10-20 01:55:21 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,7 +31,7 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
